fe9071dc9a0cef66e3d415eba578edc3c7e5f0ee
Jacob <mail@jboline.us>
2018-10-27 21:38:13 -0500

    C Code and Ignore

---
+++ ADC_Test.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_test.elf_on_local.tcl
@@ -0,0 +1,8 @@
+connect -url tcp:127.0.0.1:3121
+configparams mdm-detect-bscan-mask 2
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Genesys 2 200300A8C57EB"} -index 0
+rst -processor
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Genesys 2 200300A8C57EB"} -index 0
+dow C:/Users/Jacob/Research/ADC_Design/ADC_Test.sdk/test/Debug/test.elf
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Genesys 2 200300A8C57EB"} -index 0
+con
