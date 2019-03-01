47b7deb3551f5d844d2ee04dcb1a01bc34cfa61e
Jacob <mail@jboline.us>
2019-03-01 13:54:39 -0600

    6 Channels functional

--- ADC_Test.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_test.elf_on_local.tcl
+++
@@ -1,8 +0,0 @@
-connect -url tcp:127.0.0.1:3121
-configparams mdm-detect-bscan-mask 2
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Genesys 2 200300A8C57EB"} -index 0
-rst -processor
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Genesys 2 200300A8C57EB"} -index 0
-dow C:/Users/Jacob/Research/adc_test/ADC_Test.sdk/test/Debug/test.elf
-targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent Genesys 2 200300A8C57EB"} -index 0
-con
